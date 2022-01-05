bfcaa80ce11d8406753303382fadae246b6da367
Temiloluwa Philip Ojo <codekagei@gmail.com>
2022-01-05 22:06:10 +0100

    installed typescript in project and added prettier for code formatting

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Response, Request, NextFunction } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello");
+});
+
+app.listen(5000, () =>
+  console.log(`Server listening on http://localhost:${5000}/`)
+);
