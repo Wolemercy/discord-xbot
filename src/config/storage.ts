b3ac1d4bd71d2bddd737ae09fa46ad455048f1eb
Wolemercy <wolemercy@gmail.com>
2022-02-14 20:26:39 +0100

    :bug: Fix Cache connection

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -1,15 +1,21 @@
 import { Tedis } from 'tedis';
 import { PrismaClient } from '@prisma/client';
 
-const { NODE_ENV, CAHE_PORT, CACHE_HOST, CACHE_PASSWORD } = process.env;
+const { NODE_ENV, REDIS_URL } = process.env;
 
 const generalConfig = {
     host: '127.0.0.1',
     port: 6379
 };
+
+const split_url = REDIS_URL?.split(':');
+const CACHE_HOST = split_url![2].split('@')[1];
+const CACHE_PASSWORD = split_url![2].split('@')[0];
+const CACHE_PORT = split_url![3];
+
 const prodConfig = {
     host: CACHE_HOST,
-    port: Number(CAHE_PORT),
+    port: Number(CACHE_PORT),
     password: CACHE_PASSWORD!,
     tls: undefined
 };
