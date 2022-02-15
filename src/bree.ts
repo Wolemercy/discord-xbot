98889e8615b47233ef87e76a1dc6652a54a8021e
Wolemercy <wolemercy@gmail.com>
2022-02-15 18:14:26 +0100

    :hammer: Update notifier interval

--- src/bree.ts
+++ src/bree.ts
@@ -2,6 +2,7 @@ require('dotenv').config(); // imports the .env file to process.env
 import Bree from 'bree';
 import path from 'path';
 Bree.extend(require('@breejs/ts-worker'));
+const later = require('@breejs/later');
 
 const { TS_NODE } = process.env;
 const extention = TS_NODE ? (TS_NODE === 'true' ? 'ts' : 'js') : 'js';
@@ -15,7 +16,7 @@ const bree = new Bree({
             name: 'notifier',
             // timeout: '1s',
             // interval: 'every 30 seconds'
-            interval: 'every day at 17:01 and 18:03'
+            interval: later.parse.text('every day at 17:17 and 18:18')
         }
     ],
     acceptedExtensions: ['.ts', '.js']
