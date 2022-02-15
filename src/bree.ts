92a610e7fefbe71e247b0c8b385650caa588c5e1
Wolemercy <wolemercy@gmail.com>
2022-02-15 16:17:40 +0100

    :hammer: Update notifier interval

--- src/bree.ts
+++ src/bree.ts
@@ -14,7 +14,7 @@ const bree = new Bree({
             name: 'notifier',
             // timeout: '1s',
             // interval: 'every 30 seconds'
-            interval: 'every day at 3 am and 6 am'
+            interval: 'every day at 16:35'
         }
     ],
     acceptedExtensions: ['.ts', '.js']
